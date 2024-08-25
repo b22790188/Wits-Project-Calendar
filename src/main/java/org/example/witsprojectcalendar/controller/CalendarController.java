59bcef09a0514d674750d2f366a98f6b8c407ab2
DogeWebCode <james56012623@gmail.com>
2024-08-25 12:44:23 +0800

    [feat] complate event search and update

--- src/main/java/org/example/witsprojectcalendar/controller/CalendarController.java
+++ src/main/java/org/example/witsprojectcalendar/controller/CalendarController.java
@@ -1,12 +1,12 @@
 package org.example.witsprojectcalendar.controller;
 
+import com.google.api.services.calendar.model.Event;
 import org.example.witsprojectcalendar.data.dto.ErrorResponseDTO;
+import org.example.witsprojectcalendar.data.dto.UpdateEventRequest;
 import org.example.witsprojectcalendar.service.CalendarService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -32,6 +32,17 @@ public class CalendarController {
         boolean isDeleted = calendarService.deleteEvent(eventId);
         return isDeleted ? ResponseEntity.noContent().build()
             : ResponseEntity.badRequest().body(new ErrorResponseDTO("Delete event failed"));
+    }
+
+    @GetMapping("/events")
+    public ResponseEntity<?> getEvents() throws IOException {
+        return ResponseEntity.ok(calendarService.getEvents());
+    }
+
+    @PutMapping("/events/{eventId}")
+    public ResponseEntity<?> updateEvent(@PathVariable("eventId") String eventId, @RequestBody UpdateEventRequest request) throws IOException {
+        Event updateEvent = calendarService.updateEvent(eventId, request);
+        return ResponseEntity.ok(updateEvent);
     }
 
 }
--- src/main/java/org/example/witsprojectcalendar/service/CalendarService.java
+++ src/main/java/org/example/witsprojectcalendar/service/CalendarService.java
@@ -9,11 +9,15 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.DateTime;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.Events;
 import lombok.extern.log4j.Log4j2;
+import org.example.witsprojectcalendar.data.dto.UpdateEventRequest;
 import org.springframework.stereotype.Service;
 
 import java.io.FileNotFoundException;
@@ -67,6 +71,45 @@ public class CalendarService {
     public boolean deleteEvent(String id) throws IOException {
         service.events().delete("primary", id).execute();
         return true;
+    }
+
+    public List<Event> getEvents() throws IOException {
+        DateTime now = new DateTime(System.currentTimeMillis());
+        Events events = service.events().list("primary").setMaxResults(10).setTimeMin(now).setOrderBy("startTime").setSingleEvents(true).execute();
+        List<Event> items = events.getItems();
+        if (items.isEmpty()) {
+            System.out.println("No upcoming events found.");
+        } else {
+            System.out.println("Upcoming events");
+            for (Event event : items) {
+                DateTime start = event.getStart().getDateTime();
+                if (start == null) {
+                    start = event.getStart().getDate();
+                }
+                System.out.printf("%s (%s)\n", event.getSummary(), start);
+            }
+        }
+        return items;
+    }
+
+    public Event updateEvent(String eventId, UpdateEventRequest request) throws IOException {
+
+        log.info("Attempting to update event with ID: {}", eventId);
+
+        // 把 Id 帶入，然後取得指定的 Event
+        Event event = service.events().get("primary", eventId).execute();
+        log.info("Successfully retrieved event: {}", event.getSummary());
+
+        // 這裡放要更新的內容都塞到 event 物件裡面，目前有 Summary(標題)、Start(開始時間)、End(結束時間)
+        event.setSummary(request.getNewSummary());
+
+        event.setStart(new EventDateTime().setDateTime(new DateTime(request.getNewStart())));
+        event.setEnd(new EventDateTime().setDateTime(new DateTime(request.getNewEnd())));
+
+        // 更新 Event
+        Event updatedEvent = service.events().update("primary", eventId, event).execute();
+        log.info("Event updated successfully: {} ({})", updatedEvent.getSummary(), updatedEvent.getUpdated());
+        return updatedEvent;
     }
 
 

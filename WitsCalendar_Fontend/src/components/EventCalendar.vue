df1eba9540fa483432b69e7a4c36e6c4064c0efc
guo <b22790188@gmail.com>
2024-08-26 15:48:21 +0800

    [fea] get event with axios on EventCalendar mounted

--- WitsCalendar_Fontend/src/components/EventCalendar.vue
+++ WitsCalendar_Fontend/src/components/EventCalendar.vue
@@ -7,10 +7,12 @@ import { ref, watch, onMounted } from 'vue'
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import axios from 'axios'
 
 const emit = defineEmits(['date-click'])
 const props = defineProps(['weekends'])
 const fullCalendar = ref(null)
+const events = ref([])
 
 const calendarOptions = ref({
   plugins: [dayGridPlugin, interactionPlugin],
@@ -20,10 +22,12 @@ const calendarOptions = ref({
     console.log('Date clicked in Calendar:', arg.dateStr)
     emit('date-click', arg.dateStr)
   },
-  events: [
-    { title: '顯示測試事件 01', date: '2024-08-30' },
-    { title: '顯示測試事件 02', date: '2024-08-31' }
-  ]
+  // events: [
+  //         { title: 'event 1', date: '2019-04-01' },
+  //         { title: 'event 2', date: '2019-04-02' }
+  //       ]
+
+  events: events.value
 })
 
 watch(
@@ -37,8 +41,23 @@ watch(
   }
 )
 
-onMounted(() => {
+watch(events, (newEvents) => {
+  if (fullCalendar.value) {
+    const api = fullCalendar.value.getApi()
+    api.removeAllEvents()
+    api.addEventSource(newEvents)
+  }
+})
+
+onMounted(async () => {
   console.log('Calendar mounted')
+  try {
+    const response = await axios.get('http://localhost:8080/events')
+    console.log(response.data);
+    events.value = response.data
+  } catch (error) {
+    console.error('Failed to fetch events:', error)
+  }
 })
 
 const addEvent = (event) => {
